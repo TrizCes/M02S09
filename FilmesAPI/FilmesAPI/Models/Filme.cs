663de0754a790f8a69b7205d954abf7b812db6e9
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-29 22:05:13 -0300

    aula01

---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Diretor { get; set; }
+        public String Genero { get; set; }
+        public int Duracao { get; set; }
+    }
+}
