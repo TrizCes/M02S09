793662031d1c3c526c4be7a03057f977834212e7
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-30 22:19:16 -0300

    Implementação Get/:id - Post - Put

--- FilmesAPI/FilmesAPI/Models/Filme.cs
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models
 {
     public class Filme
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é de preenchimento obrigatório")]
+        [MaxLength(200, ErrorMessage = "O campo nome não pode exceder 200 caracteres")]
         public String Nome { get; set; }
         public String Diretor { get; set; }
         public String Genero { get; set; }
